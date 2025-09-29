acfe175039759f7efe48a3be9ea5788aa71dc41b
LuukvdBilt <338255@student.mboutrecht.nl>
2025-09-29 10:31:06 +0200

    aflevering 8&9 gedaan

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,39 +1,11 @@
 <?php
-
 namespace App\Models;
 
-use Illuminate\Support\Arr;
 
-class Job {
-    public static function all(): array
-    {
-        return [
-    
-            [
-                'id' => 1,
-                'title' => 'Software Engineer',
-                'salary' => '120000',
-            ]
-            ,
-            [
-                'id' => 2,
-                'title' => 'Data Scientist',
-                'salary' => '130000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Product Manager',
-                'salary' => '110000',
-            ]
-        
-            ];
-    }
- public static function find(int $id): array
-{
-    $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-    if (! $job) {
-        abort(404);
-    }
-    return $job;
-}
+use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model as Models;
+
+class Job extends Models {
+    protected $table = 'job_listings';
+    protected $fillable = ['title', 'salary'];
 }
