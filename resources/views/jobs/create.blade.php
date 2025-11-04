3c5f72459157401de5fe774ba84c3f994f6f741b
LuukvdBilt <338255@student.mboutrecht.nl>
2025-11-04 11:28:53 +0100

    19&20&21

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -13,7 +13,7 @@
 
       <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
         <div class="sm:col-span-4">
-          <label for="Title" class="block text-sm/6 font-medium text-white">Title</label>
+          <x-form-label for="title">Title</x-form-label>
           <div class="mt-2">
             <div class="flex items-center rounded-md bg-white/5 pl-3 outline-1 -outline-offset-1 outline-white/10 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-500">
               <div class="shrink-0 text-base text-gray-400 select-none sm:text-sm/6">Shift Leader</div>
